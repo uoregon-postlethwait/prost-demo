8cbe4407a6d9ba62a3ab5582bfc98c62c7ed62e9
Jason Sydes <sydes@uoregon.edu>
2019-09-17 16:14:50 -0700

    Switch to more stable Ensembl archive version of GRCz10 zebrafish assembly.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@
 set -e
 
 # Is BBMap in PATH?
-if [ ! `which bbmap.sh 2>/dev/null` ] ; then 
+if [ ! `which bbmap.sh 2>/dev/null` ] ; then
     echo Error: Cannot find bbmap.sh in your \$PATH.
     echo Please add the directory containing your download of BBMap to your \$PATH, like so:
     echo
@@ -16,7 +16,7 @@ if [ ! `which bbmap.sh 2>/dev/null` ] ; then
     exit
 fi
 
-# This script needs to be run 
+# This script needs to be run
 if [ ! -d fa ] || [ ! -d samples ] ; then i
     echo Error: Please run this script from the top directory.
     echo In other words, run this script like so:
@@ -31,19 +31,22 @@ if [[ `ls fa/*.gz 2>/dev/null` ]] ; then gunzip fa/*.gz ; fi
 if [[ `ls samples/*.gz 2>/dev/null` ]] ; then gunzip samples/*.gz ; fi
 
 # Get the ZF assembly.
+ASSEMBLY=ftp://ftp.ensembl.org/pub/release-90/fasta/danio_rerio/dna/Danio_rerio.GRCz10.dna.toplevel.fa.gz
 if [ -e Danio_rerio.GRCz10.dna.toplevel.fa.gz ] ; then
     # Already downloaded: noop.
     :
-elif [ `which wget` ] ; then 
+elif [ `which wget` ] ; then
     echo "Downloading Danio_rerio.GRCz10.dna.toplevel.fa.gz.  This may take a few minutes..."
-    wget ftp://ftp.ensembl.org/pub/current_fasta/danio_rerio/dna/Danio_rerio.GRCz10.dna.toplevel.fa.gz 2> /dev/null
-elif [ `which curl` ] ; then 
+    wget $ASSEMBLY 2> /dev/null || \
+        echo "Error, couldn't download $ASSEMBLY (using wget)." && exit 1
+elif [ `which curl` ] ; then
     echo "Downloading Danio_rerio.GRCz10.dna.toplevel.fa.gz.  This may take a few minutes..."
-    curl -O ftp://ftp.ensembl.org/pub/current_fasta/danio_rerio/dna/Danio_rerio.GRCz10.dna.toplevel.fa.gz 2> /dev/null
-else 
+    curl -O $ASSEMBLY 2> /dev/null || \
+        echo "Error, couldn't download $ASSEMBLY (using curl)." && exit 1
+else
     echo "Error: Your system does not have 'wget' or 'curl' installed, so I cannot"
     echo "       download Danio_rerio.GRCz10.dna.toplevel.fa.gz for you. Please retrieve"
-    echo "       the file //ftp.ensembl.org/pub/current_fasta/danio_rerio/dna/Danio_rerio.GRCz10.dna.toplevel.fa.gz"
+    echo "       the file $ASSEMBLY"
     echo "       and place it in the current directory, then try again."
     echo Exiting.
     exit
@@ -54,7 +57,7 @@ echo "Building BBMap database of Danio_rerio.GRCz10.dna.toplevel.fa.gz.  This ma
 echo "    If you wish to run Prost! on a species besides zebrafish, you'll need to be able to "
 echo "    build your own BBMap databases. For your reference, here's how I'm building "
 echo "    the zebrafish BBMap database:"
-echo 
+echo
 echo "    bbmap.sh k=7 path=BBMap/Danio_rerio.GRCz10.dna.toplevel ref=Danio_rerio.GRCz10.dna.toplevel.fa.gz"
 echo
 echo "Please be patient while I build the zebrafish BBMap database in your current directory..."
